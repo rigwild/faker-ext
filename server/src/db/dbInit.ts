b104b523fa2c5d054ea288ba1f04f799430a2a88
rigwild <me@rigwild.dev>
2022-02-17 19:45:27 +0100

    server: Add hello world post + update public dir

--- server/src/db/dbInit.ts
+++ server/src/db/dbInit.ts
@@ -2,14 +2,15 @@ import Media from './models/media.model'
 import Post from './models/post.model'
 import ApiUser from './models/apiUser.model'
 import { apiUserService } from '../services/apiUser.service'
+import { postService } from '../services/post.service'
 
 const dbInit = async () => {
   await Post.sync({ alter: true })
   await Media.sync({ alter: true })
   await ApiUser.sync({ alter: true })
 
-  // Create an initial user
   await createUser()
+  await createHelloWorldPost()
 }
 
 const createUser = async () => {
@@ -19,6 +20,14 @@ const createUser = async () => {
   if (username && password && !(await apiUserService.existsByUsername(username))) {
     console.log(`Creating initial user "${username}"`)
     await apiUserService.createUser(username, password)
+  }
+}
+
+const createHelloWorldPost = async () => {
+  const id = '12345678-abcd-4abc-abcd-123456789abc'
+  if (!(await postService.getPostById(id))) {
+    console.log(`Creating hello world post`)
+    await postService.createPost({ id, content: 'Hello world!' })
   }
 }
 
